0709cefea52bc74abe369be974e7b5236bbc0bb7
Kalharz <naveenvirajkalhara.na@gmail.com>
2026-08-20 22:56:01 +0530

    Spring files initialized and refactored

--- backend/src/main/java/com/busfleetmanagement/system/BusFleetManagementApplication.java
+++ backend/src/main/java/com/busfleetmanagement/system/BusFleetManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SystemApplication {
+public class BusFleetManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SystemApplication.class, args);
+		SpringApplication.run(BusFleetManagementApplication.class, args);
 	}
 
 }
