4351c92f11829f50bbea7c8178f91b90324eaf56
navee7274 <naveenvirajkalhara.na@gmail.com>
2026-08-26 18:35:08 +0530

    Project Updated

--- backend/src/main/java/com/busfleetmanagement/system/SystemApplication.java
+++ backend/src/main/java/com/busfleetmanagement/system/SystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BusFleetManagementApplication {
+public class SystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BusFleetManagementApplication.class, args);
+		SpringApplication.run(SystemApplication.class, args);
 	}
 
 }
