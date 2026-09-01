f1f0e7f44a0e672fbe16fe178aaea7150fad01f2
WageeshaEdirisinghe <it25102771@my.sliit.lk>
2026-09-01 12:21:24 +0530

    drivers_table

--- database/M2_Wageesha.sql
+++ database/M2_Wageesha.sql
@@ -1,2 +1,12 @@
 USE bus_fleet_management_001;
 
+CREATE TABLE Driver (
+    DriverID CHAR(10) PRIMARY KEY,
+    DriverNo VARCHAR(100) NOT NULL,
+    DriverNIC VARCHAR(20),
+    DContactNo VARCHAR(20),
+    BaseMonthlySalary DECIMAL(10,2) NOT NULL,
+    PerTripAllowence DECIMAL(10,2) NOT NULL,
+    notes VARCHAR(500)
+);
+SELECT * FROM Driver;
