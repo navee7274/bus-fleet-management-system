ce24d19373df7f5ceefb64eee130743637a6de07
WageeshaEdirisinghe <it25102771@my.sliit.lk>
2026-09-24 12:43:47 +0530

    updated driver.java

--- database/M2_Wageesha.sql
+++ database/M2_Wageesha.sql
@@ -2,13 +2,15 @@ USE bus_fleet_management_001;
 
 CREATE TABLE Driver (
     DriverID CHAR(10) PRIMARY KEY,
-    DriverNo VARCHAR(100) NOT NULL,
-    DriverNIC VARCHAR(20),
-    DContactNo VARCHAR(20),
+    Name VARCHAR(100) NOT NULL,
+    NIC VARCHAR(20),
+    ContactNo VARCHAR(20),
     BaseMonthlySalary DECIMAL(10,2) NOT NULL,
     PerTripAllowence DECIMAL(10,2) NOT NULL,
     notes VARCHAR(500)
 );
+ALTER TABLE Driver ADD COLUMN BCapacity INT AFTER BPurchasePrice;
+
 SELECT * FROM Driver;
 
 CREATE TABLE Driver_Salary (
