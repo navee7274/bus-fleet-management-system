492a83bd07288523b6d144fd01499b283a83343a
KLHTKothalawala <it25102761@my.sliit.lk>
2026-09-24 15:42:53 +0530

    LoginResponse.java updated

--- backend/src/main/java/com/busfleetmanagement/system/dto/LoginResponse.java
+++ backend/src/main/java/com/busfleetmanagement/system/dto/LoginResponse.java
@@ -1,4 +1,32 @@
 package com.busfleetmanagement.system.dto;
 
 public class LoginResponse {
+
+    private String message;
+    private Integer ownerId;
+    private String username;
+    private String name;
+
+    public LoginResponse(String message, Integer ownerId, String username, String name){
+        this.message = message;
+        this.ownerId = ownerId;
+        this.username = username;
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Integer getOwnerId() {
+        return ownerId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
