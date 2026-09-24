6f7e48c33ff7b86c31e2d64ae24753d71ec299a2
KLHTKothalawala <it25102761@my.sliit.lk>
2026-09-24 13:28:32 +0530

    Owner, ExpenseLog, MaintananceLog Tables are created

--- database/M3 Hiruni.sql
+++ database/M3 Hiruni.sql
@@ -1,38 +1,40 @@
 USE bus_fleet_management_001;
 
-CREATE TABLE Journey(
-JourneyID INT PRIMARY KEY,
-JDate DATE NOT NULL,
-JBusNo VARCHAR(10) NOT NULL,
-JDriver VARCHAR(5) NOT NULL,
-JPurpose VARCHAR(20) NOT NULL,
-Jclient VARCHAR(10) ,
-JDeparture VARCHAR(50),
-JDestination VARCHAR(50),
-JStartOdo INT NOT NULL,
-JEndOdo INT NOT NULL,
-JKMTravelled  INT GENERATED ALWAYS AS (JEndOdo - JStartOdo) STORED,
-JIncomeAmount DECIMAL(10,2) NOT NULL,
-JNotes VARCHAR(100),
-CONSTRAINT ST1 CHECK( JStartOdo > 0),
-CONSTRAINT ST2 CHECK( JEndOdo > 0),
-CONSTRAINT ST3 CHECK( JEndOdo > JStartOdo ),
-CONSTRAINT ST4 CHECK( JPurpose IN('Staff Transport', 'Tour', 'Hire', 'Other'))
+CREATE TABLE Owner (
+    OwnerID INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    Password VARCHAR(255) NOT NULL,
+    Name VARCHAR(100) NOT NULL,
+    ContactNo VARCHAR(20)
 );
 
-CREATE TABLE Expence(
-Edate DATE NOT NULL,
-EbusRegNo VARCHAR(20) NOT NULL,
-ECategory VARCHAR(20) NOT NULL,
-EPaymentMethod VARCHAR(20) NOT NULL,
-EAmount DECIMAL(10,2),
-EDescription VARCHAR(50),
+
+CREATE TABLE ExpenceLog(
+ExpenseID INT AUTO_INCREMENT PRIMARY KEY,
+Expensedate DATE NOT NULL,
+busRegNo VARCHAR(20) NOT NULL,
+Category VARCHAR(20) NOT NULL,
+PaymentMethod VARCHAR(30) NOT NULL,
+Amount DECIMAL(10,2),
+ExpenseDescription VARCHAR(50),
 CONSTRAINT ET1 CHECK(EAmount > 0),
 CONSTRAINT ET2 CHECK( ECategory IN ('Insurance', 'Tires', 'Spare Parts', 'Other Staff Wages', 'Other')),
-CONSTRAINT ET3 CHECK( EPaymentMethod IN('Cash', 'Bank Transfer', 'Cheque', 'Credit', 'Other'))
+CONSTRAINT ET3 CHECK( EPaymentMethod IN('Cash', 'Bank Transfer', 'Cheque', 'Credit', 'Other')),
+CONSTRAINT fk_expense_bus FOREIGN KEY (BRegistrationNo) REFERENCES Bus(BRegistrationNo)
 );
 
- 
+CREATE TABLE MaintenanceLog (
+MaintenanceID INT AUTO_INCREMENT PRIMARY KEY,
+MaintenanceDate DATE NOT NULL,
+BRegistrationNo VARCHAR(20) NOT NULL,
+MType VARCHAR(30) NOT NULL,
+MDescription VARCHAR(255) NOT NULL,
+Cost DECIMAL(10,2) NOT NULL,
+Odometer DECIMAL(10,2),
+NextServiceDue VARCHAR(100),
+CONSTRAINT fk_maintenance_bus FOREIGN KEY (BRegistrationNo) REFERENCES Bus(BRegistrationNo),
+CONSTRAINT MT1 CHECK( MType IN ('Service' , 'Repair'))
+);
 
 DESCRIBE Journey;
 
