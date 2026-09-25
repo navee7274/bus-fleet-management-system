aacdd1aa864be99e2b40264f14931dcac546e720
Kalharz <naveenvirajkalhara.na@gmail.com>
2026-09-25 11:18:33 +0530

    Maintenance Booking and Payment Repositories Made

--- backend/src/main/java/com/busfleetmanagement/system/entity/MaintenanceLog.java
+++ backend/src/main/java/com/busfleetmanagement/system/entity/MaintenanceLog.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "MaintenanceLog")
--- backend/src/main/java/com/busfleetmanagement/system/repository/MaintenanceLogRepository.java
+++ backend/src/main/java/com/busfleetmanagement/system/repository/MaintenanceLogRepository.java
@@ -1,4 +1,7 @@
 package com.busfleetmanagement.system.repository;
 
-public interface MaintenanceLogRepository {
+import com.busfleetmanagement.system.entity.MaintenanceLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaintenanceLogRepository extends JpaRepository<MaintenanceLog, Integer> {
 }
